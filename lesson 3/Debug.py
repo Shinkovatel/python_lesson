068201b8f85a3bf7eaa48f59400303daf844f282
shinkovatel <bsn1980@yandex.ru>
2019-12-06 16:10:05 +0300

    Работа со словарем

---
+++ lesson 3/Debug.py
@@ -0,0 +1,6 @@
+friend = {
+    'name': 'max',
+    'age': 38}
+
+print(friend)
+print(type(friend))
---
+++ lesson 3/lesson 3.12.py
@@ -0,0 +1,16 @@
+# dict
+# неупорядочные коллекции произвольных объектов с доступом по ключу
+my_dict = {key1: val1, key2: val2}
+
+dog = {'name':'Rocky', age: 3}
+
+friends = ['Max', 'leo', 'Kate']
+
+# Как добавить возраст другу
+
+friend = {
+    'name': 'max',
+    'age': 38}
+
+print(friend)
+print(type(friend))
