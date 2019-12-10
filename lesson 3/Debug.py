58846551df18804686e56db617a59986e5f92ded
shinkovatel <bsn1980@yandex.ru>
2019-12-10 17:27:58 +0300

    Работа со словарем часть 2

--- lesson 3/Debug.py
+++ lesson 3/Debug.py
@@ -1,6 +1,19 @@
 friend = {
     'name': 'max',
-    'age': 38}
+    'age': 38,
+    'has car': True}
 
-print(friend)
-print(type(friend))
+# По ключам
+
+for key in friend.keys():
+    print(key)
+
+for val in friend.values():
+    print(val)
+
+for item in friend.items():
+    print(item)
+
+for key, val in friend.items():
+    print(key)
+    print(val)
--- lesson 3/lesson 3.12.py
+++ lesson 3/lesson 3.12.py
@@ -14,3 +14,46 @@ friend = {
 
 print(friend)
 print(type(friend))
+
+print(friend['age']) # Узнаем сколько лет нашему другу
+
+friend['has_car'] = True # Добавляем ключ и значение в словарь
+print(friend)
+
+friend['has_car'] = False # Произсходит замена, т.к. с одним ключем может быть только одно значение
+print(friend)
+
+del  friend['age'] # Удаляем сразу пару, ключ и значение
+
+if 'age' in friend: # Используется in
+    print('Есть возраст')
+
+# Перебор словаря по: Ключам, Значениям, Парам ключ/значение
+
+friend = {
+    'name': 'max',
+    'age': 38,
+    'has car': True}
+
+# По ключам
+
+for key in friend.keys():
+    print(key)
+    print(friend[key]) # по значениям
+
+for key in friend: # альтернативный вариант
+    print(key)
+    print(friend[key]) # по значениям
+
+# По значениям
+
+for val in friend.values():
+    print(val)
+
+# Пара ключ-значение
+
+for item in friend.items(): # Кортеж
+    print(item)
+for key, val in friend.items():
+    print(key)
+    print(val)
